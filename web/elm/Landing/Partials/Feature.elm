2d5847ff8c496201be1bc4d5c80adcbef039cd89
nicolas <nicolas.goumard@gmail.com>
2017-04-27 20:57:29 +0200

    Refactored landing.

--- web/elm/Landing/Partials/Feature.elm
+++ web/elm/Landing/Partials/Feature.elm
@@ -1,21 +1,46 @@
-module Landing.Partials.Feature exposing (featureView)
+module Landing.Partials.Feature exposing (renderFeatures)
 import Html exposing (..)
 import Html.Events exposing (..)
 import Html.Attributes exposing (..)
 
-featureView : String -> String -> String -> String -> Html a
-featureView title text_ linkText linkTo =
+type alias FeatureItem = {
+  title : String
+  , text : String
+  , linkText : String
+  , linkTo : String
+}
+
+type alias FeatureItems = List FeatureItem
+
+features : FeatureItems
+features =
+  [
+    { title = "Feature", text = "Our tools empower you to grow your business in a way only you can.", linkText = "Explore Features", linkTo = "/features"}
+    , { title = "Pricing", text = "Start sending emails today for free. Then level up as you grow.", linkText = "View Pricing", linkTo = "/pricing"}
+    , { title = "E-Commerce", text = "Recapture sales, recommend products, and make money in your sleep.", linkText = "Connect Your Store", linkTo = "/connect"}
+  ]
+
+renderFeature : FeatureItem -> Html a
+renderFeature feature =
   div [ class "feature" ]
       [ h2 [ class "title" ]
         [
-          text title
+          text feature.title
         ]
         , div [ class "text" ]
           [
-            text text_
+            text feature.text
           ]
-        , a [ class "shared-link", href linkTo ]
+        , a [ class "shared-link", href feature.linkTo ]
           [
-            text linkText
+            text feature.linkText
           ]
       ]
+
+renderFeatures : Html a
+renderFeatures =
+  let
+    featureItemsHtml = List.map renderFeature features
+  in
+    div [ class "landing-features" ]
+    (featureItemsHtml)
